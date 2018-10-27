4de3f296fa4b108d3047d23c12280f9ff7dc2042
WillForan <willforan@gmail.com>
2018-10-27 11:28:59 -0400

    add colorschemes

---
+++ colorschemes/Tartan.sh
@@ -0,0 +1,34 @@
+# Tartan colours
+
+# urxvt color scheme:
+
+background="#2B2B2B"
+foreground="#DEDEDE"
+
+colorUL="#86a2b0"
+
+#  black
+color0="#2E3436"
+color8="#555753"
+#  red
+color1="#CC0000"
+color9="#EF2929"
+#  green
+color2="#4E9A06"
+color10="#8AE234"
+#  yellow
+color3="#C4A000"
+color11="#FCE94F"
+#  blue
+color4="#3465A4"
+color12="#729FCF"
+#  magenta
+color5="#75507B"
+color13="#AD7FA8"
+#  cyan
+color6="#06989A"
+color14="#34E2E2"
+#  white
+color7="#D3D7CF"
+color15="#EEEEEC"
+# from www.xcolors.net/dl/Tartan
---
+++ colorschemes/derp.sh
@@ -0,0 +1,33 @@
+#  Black
+color0="#111111"
+color8="#666666"
+
+#  Red
+color1="#d36265"
+color9="#ef8171"
+
+#  Green
+color2="#aece91"
+color10="#cfefb3"
+
+#  Yellow
+color3="#e7e18c"
+color11="#fff796"
+
+#  Blue
+color4="#5297cf"
+color12="#74b8ef"
+
+#  Magenta
+color5="#963c59"
+color13="#b85e7b"
+
+#  Cyan
+color6="#5E7175"
+color14="#A3BABF"
+
+#  White
+color7="#bebebe"
+color15="#ffffff"
+
+#  vim: et sw=2 syn=xdefaults
