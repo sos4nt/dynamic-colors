1fdca2c326ac56a2585dc4f4e3ff18e6b7f79f4b
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-03-19 11:27:47 +0100

    Add rovanion color schemes

---
+++ colorschemes/rovanion-dark.sh
@@ -0,0 +1,36 @@
+# Rovanion's dark theme, a high contrast theme.
+black="#000000"
+white="#ffffff"
+blue="#0d38cc"       # color4
+brblue="#0f75ff"     # color12
+
+background="$black"
+foreground="$white"
+cursor="$white"
+mouse_background="$black"
+mouse_foreground="$white"
+highlight="$white"
+
+ color0="black"      # black
+ color8="brblack"    # brblack
+
+ color1="red"        # red
+ color9="brred"      # brred
+
+ color2="green"      # green
+color10="brgreen"    # brgreen
+
+ color3="yellow"     # yellow
+color11="bryellow"   # bryellow
+
+ color4="$blue"      # blue
+color12="$brblue"    # brblue
+
+ color5="magenta"    # magenta
+color13="brmagenta"  # brmagenta
+
+ color6="cyan"       # cyan
+color14="brcyan"     # brcyan
+
+ color7="white"      # white
+color15="brwhite"    # brwhite
